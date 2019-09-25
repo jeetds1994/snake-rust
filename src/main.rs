17db1ae26393515d0364a1d6288df3e7464ac1f0
jeetds1994 <jeetds1994@gmail.com>
2019-09-24 22:23:16 -0400

    food is now displayed along with snake

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ extern crate piston;
 extern crate graphics;
 extern crate glutin_window;
 extern crate opengl_graphics;
+extern crate rand;
 
 use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
+use rand::Rng;
 
 use std::collections::LinkedList;
 use std::iter::FromIterator;
@@ -17,10 +19,10 @@ enum Direction {
     Right, Left, Up, Down
 }
 
-
 struct Game {
     gl: GlGraphics,
-    snake: Snake
+    snake: Snake,
+    food: Food
 }
 
 impl Game {
@@ -35,6 +37,17 @@ impl Game {
         });
 
         self.snake.render(&mut self.gl, arg);
+
+        self.food.render(&mut self.gl, arg);
+
+        self.check_for_collision();
+    }
+
+    fn check_for_collision(&mut self) {
+        print!("{:?}", self.snake.body.front());
+        // if self.food.pos_x == self.snake.body.front( && self.food.pos_y == self.snake.body.front().1 {
+        //     print!("collision");
+        // }
     }
 
     fn update(&mut self) {
@@ -55,6 +68,29 @@ impl Game {
                 if last_direction != Direction::Left => Direction::Right,
             _ => last_direction
         };
+    }
+}
+
+struct Food {
+    pos_x: i32,
+    pos_y: i32
+}
+
+impl Food {
+    fn render(&self, gl: &mut GlGraphics, arg: &RenderArgs) {
+
+        use graphics;
+
+        let BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+
+        let food = graphics::rectangle::square((self.pos_x * 20) as f64, (self.pos_y * 20) as f64, 20_f64);
+
+        gl.draw(arg.viewport(), |c, gl| {
+
+            let transform = c.transform;
+            graphics::rectangle(BLUE, food, transform, gl);
+
+        })
     }
 }
 
@@ -115,12 +151,15 @@ fn main() {
     .build()
     .unwrap();
 
+    let mut rng = rand::thread_rng();
+
     let mut game = Game {
         gl: GlGraphics::new(opengl),
         snake: Snake {
             dir: Direction::Right,
             body: LinkedList::from_iter((vec![(0,0), (0, 1)]).into_iter())
-        }
+        },
+        food: Food { pos_x: rng.gen_range(0, 400 / 20), pos_y: rng.gen_range(0, 400 / 20) }
     };
 
 
